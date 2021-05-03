d7b9ebae7ce8c144df65cd386be6e616246b9abb
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:26:07 +0800

    Problem 2 completed

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,21 +1,21 @@
-package assignment;
-
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Problem2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        InputReader refReader = new InputReader();
-        int numOfRows = refReader.getNumOfRows();
+        // Get the number of rows from the user input
+        Scanner refScanner = new Scanner(System.in);
+        System.out.println("How many rows?");
+        int numOfRows = refScanner.nextInt();
 
-        int currentNumber = 1;
-        for(int i = 1; i <= numOfRows; i++){
+        int currentNumber = 1; // starts with 1
+        for(int i = 1; i <= numOfRows; i++){ // index ends with user input number
             for(int j = 0; j < i; j++ ){
                 System.out.print(currentNumber);
-                currentNumber++;
-            }
-            currentNumber = 1;
+                currentNumber++; // increment by 1
+            } // end of inner loop
+            currentNumber = 1; // every row must start with 1
             System.out.println();
-        }
+        } // end of outer loop
     }
 }
