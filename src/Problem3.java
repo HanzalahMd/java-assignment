918d1814de9bc70a4c8997c485e0283264cdb06a
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:30:00 +0800

    problem 3 completed & description added

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,22 +1,29 @@
-package assignment;
+// Problem 3
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Problem3 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        InputReader refReader = new InputReader();
-        int numOfRows = refReader.getNumOfRows();
+        // Get the number of rows from the user input
+        Scanner refScanner = new Scanner(System.in);
+        System.out.println("How many rows?");
+        int numOfRows = refScanner.nextInt();
 
         int asciiValue = 65; // Initial value is 65 which is 'A'
-        for(int i = 1; i <= numOfRows; i++){
+        for(int i = 1; i <= numOfRows; i++){ // index ends with user input number
             for(int j = 0; j < i; j++ ){
                 char currentChar = (char) asciiValue; // convert ascii to char.
                 System.out.print(currentChar);
-            }
+            } // end of inner loop
             asciiValue++; // increment the ascii value to change alphabet
             System.out.println();
-        }
+        } // end of outer loop
     }
 }
