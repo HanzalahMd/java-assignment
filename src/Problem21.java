b90a6705155e2f12ac635ae49a343e2ce692cc21
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:46:10 +0800

    problem 21 completed

--- src/Problem21.java
+++ src/Problem21.java
@@ -1,10 +1,11 @@
-package assignment;
+// Math Power
 
 import java.util.Scanner;
 
 public class Problem21 {
     public static void main(String[] args) {
         Scanner refScanner = new Scanner(System.in);
+        System.out.println("Welcome to Math Power!!!");
         System.out.println("Enter a number");
         int number = refScanner.nextInt();
         int finalTotal = 0;
