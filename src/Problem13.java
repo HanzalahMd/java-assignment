36b544ebe7f46efef01ce4601c16229b1a29abd9
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:44:06 +0800

    problem 13 completed

--- src/Problem13.java
+++ src/Problem13.java
@@ -1,4 +1,5 @@
-package assignment;
+// Problem 13
+// return the length of the non-duplicate array
 
 import java.util.ArrayList;
 
@@ -6,16 +7,16 @@ public class Problem13 {
 
     public static void main(String[] args) {
 
-        int[] duplicateList = {20, 20, 30, 40, 50, 50, 50};
+        int[] duplicateList = {20, 20, 30, 40, 50, 50, 50}; // create sample array
 
         ArrayList<Integer> noDuplicateList = new ArrayList<>(); // Create new temporary list
 
-        for (int element:duplicateList) { // Check every element in a list
-            if(!noDuplicateList.contains(element)){ //check for duplicates,
+        for (int element:duplicateList) { // Check every element in the sample array
+            if(!noDuplicateList.contains(element)){ // check for duplicates,
                 noDuplicateList.add(element); // only add the element if it's not exists.
             }
         }
 
-        System.out.println(noDuplicateList.size());
+        System.out.println(noDuplicateList.size()); // prints the length of the noDuplicate array
     }
 }
