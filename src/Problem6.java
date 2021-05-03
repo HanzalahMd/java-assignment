83b3e12bb2171a399a41e7dc2f7cb65b053b8ff8
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:38:46 +0800

    problem 6 completed & description added

--- src/Problem6.java
+++ src/Problem6.java
@@ -1,21 +1,28 @@
-package assignment;
+// Problem 6 (Stars with spaces between each loop)
+// *
+// **
+// ****
+// ******
+// ********
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Problem6 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        InputReader refReader = new InputReader();
-        int numOfRows = refReader.getNumOfRows();
+        // Get the number of rows from the user input
+        Scanner refScanner = new Scanner(System.in);
+        System.out.println("How many rows?");
+        int numOfRows = refScanner.nextInt();
 
-        for(int i = 1; i <= numOfRows; i++){
+        for(int i = 1; i <= numOfRows; i++){ // index ends with user input number
             if(i == 1 || i % 2 == 0){ // prints only 1 and number divisible by 2.
                 for(int j = 0; j < i; j++ ){
                     System.out.print("*");
-                }
+                } // end of inner loop
                 System.out.println("\n"); // prints 1 empty line
-            }
+            } // end of outer loop
         }
     }
 }
