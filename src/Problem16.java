1a91baf2cf0149365772136cdf3fc6a08bd440ec
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:44:31 +0800

    problem 16 completed

--- src/Problem16.java
+++ src/Problem16.java
@@ -2,22 +2,23 @@
 // Generate Fibonacci Series.
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
 
-package assignment;
-
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Problem16 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        InputReader refReader = new InputReader();
-        int numOfRows = refReader.getNumOfRows();
+        // Get the number of rows from the user input
+        Scanner refScanner = new Scanner(System.in);
+        System.out.println("Welcome to Fibonaci Generator!!");
+        System.out.println("How many rows?");
+        int numOfRows = refScanner.nextInt();
 
         int prevNumber = 0;
         int currentNumber = 0;
         int newNumber; // this will be calculated with the sum of current and previous number.
 
-        for(int i = 0; i < numOfRows; i++){
+        for(int i = 0; i < numOfRows; i++){ // ends with the user input row
             if (i < 2){ // Prints the first 2 starting number
                 System.out.println(i);
                 currentNumber = i;
