37bec9aab3ce6507276aeb3d96787d6b3dc65a0b
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-03 12:40:48 +0800

    problem 8 completed

--- src/Problem8.java
+++ src/Problem8.java
@@ -1,20 +1,27 @@
-package assignment;
+// Problem 8
+// ********
+// ******
+// ****
+// **
+// *
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Problem8 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        InputReader refReader = new InputReader();
-        int numOfRows = refReader.getNumOfRows();
+        // Get the number of rows from the user input
+        Scanner refScanner = new Scanner(System.in);
+        System.out.println("How many rows?");
+        int numOfRows = refScanner.nextInt();
 
-        for(int i = numOfRows; i >= 0; i--) {
-            if (i == 1 || i % 2 == 0) {
+        for(int i = numOfRows; i >= 0; i--) { // index starts with user input number
+            if (i == 1 || i % 2 == 0) { // skips the index 1 and index divisible by 2
                 for (int j = i; j > 0; j--) {
                     System.out.print("*");
-                }
+                } // end of inner loop
                 System.out.println("\n");
             }
-        }
+        } // end of outer loop
     }
 }
